a8d33567387b63aa19ffcbb888b3e9652e480a1d
unknown <kirklayer@gmail.com>
2019-05-23 22:37:03 +0900

    guitar chords

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
 import React, { Component } from 'react';
 import ScrollMenu from 'react-horizontal-scrolling-menu';
+
+
+import chords from './chords'
+
 import './App.css';
+
 
 // list of items
 const list1 = [
-  { name: 'A1' },
-  { name: 'A2' },
-  { name: 'A3' },
-  { name: 'A4' },
-  { name: 'A5' },
-  { name: 'A6' },
-  { name: 'A7' },
-  { name: 'A8' },
-  { name: 'A9' }
+  { name: 'A' },
+  { name: 'B' },
+  { name: 'C' },
+  { name: 'D' },
+  { name: 'E' },
+  { name: 'F' },
+  { name: 'G' },
 ];
 
 const list2 = [
-  { name: 'B1' },
-  { name: 'B2' },
-  { name: 'B3' },
-  { name: 'B4' },
-  { name: 'B5' },
-  { name: 'B6' },
-  { name: 'B7' },
-  { name: 'B8' },
-  { name: 'B9' }
+  { name: 'Major' },
+  { name: 'minor' },
+  { name: '7' },
+  { name: 'm7' },
+  { name: 'maj7' },
 ];
+
+const chordsList = {
+  A:{
+    Major:{
+      name:'A',
+      positions:'X02220',
+      fingers:'--123-'
+    },
+    minor:{
+      name:'Am',
+      positions:'X02210',
+      fingers:'--231-'
+    }
+  },
+  B:{
+
+  },
+  C:{
+
+  }
+}
 
 // One item component
 // selected prop will be passed
@@ -54,11 +74,12 @@ const Arrow = ({ text, className }) => {
 };
 
 
+
 const ArrowLeft = Arrow({ text: '<', className: 'arrow-prev' });
 const ArrowRight = Arrow({ text: '>', className: 'arrow-next' });
 
-const selected1 = 'item1';
-const selected2 = 'item2';
+const selected1 = 'A';
+const selected2 = 'Major';
 
 class App extends Component {
   constructor(props) {
@@ -66,21 +87,55 @@ class App extends Component {
     // call it again if items count changes
     this.menuItems1 = Menu(list1, selected1);
     this.menuItems2 = Menu(list2, selected2);
+
+    this.currentChord = chordsList.A.Major
+
+   
   }
 
   state = {
     selected1,
-    selected2
+    selected2,
+    currentChord : chordsList.A.minor
   };
 
   onSelect1 = key => {
+    
     this.setState({ selected1: key });
     console.log(key + this.state.selected2)
+    this.currentChord = chordsList[key][this.state.selected2]
+    console.log(this.currentChord)
+    
+    setTimeout(this.change, 1)
   }
 
   onSelect2 = key => {
+    
     this.setState({ selected2: key });
     console.log(this.state.selected1 + key)
+    this.currentChord = chordsList[this.state.selected1][key]
+    console.log(this.currentChord)
+    
+    
+    
+    setTimeout(this.change, 1)
+  }
+
+  resetCanvas = () => {
+    var tmp = document.querySelectorAll('canvas')
+    tmp.forEach((item,index)=>{
+      item.remove();
+    })
+  }
+
+  change = () => {
+    this.resetCanvas()
+    chords.replace()
+  }
+
+
+  componentDidMount(){
+    chords.replace()
   }
 
 
@@ -106,6 +161,10 @@ class App extends Component {
           selected={selected2}
           onSelect={this.onSelect2}
         />
+
+        
+        
+        <chord name={this.currentChord.name} positions={this.currentChord.positions} fingers={this.currentChord.fingers} size="10" ></chord>
       </div>
     );
   }
